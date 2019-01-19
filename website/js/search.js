704dd2a4f5ab4bc65f73919d55227be26f7993b5
Usein Mambediiev <useinm@gmail.com>
2019-01-19 06:31:19 -0800

    renamed request(html/js) to search

--- website/js/search.js
+++ website/js/search.js
@@ -12,5 +12,12 @@ var Crawler = window.Crawler || {};
         alert(error);
         window.location.href = '/login.html';
     });
-    
+    function randomId() {
+        var id = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for (var i = 0; i < 6; i++) {
+            id += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return id;
+    }
 }(jQuery));
