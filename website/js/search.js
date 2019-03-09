fd8f15186f1c8220598fa555d1241b1ba31dbca3
Usein Mambediiev <useinm@gmail.com>
2019-03-09 10:31:57 -0800

    working on search requests editing UI

--- website/js/search.js
+++ website/js/search.js
@@ -1,6 +1,6 @@
 var Crawler = window.Crawler || {};
 
-(function scopeWrapper($) {
+(function scopeWrapper($, ko) {
     var authToken;
     Crawler.authToken.then(function setAuthToken(token) {
         if (token) {
@@ -14,7 +14,7 @@ var Crawler = window.Crawler || {};
     });
 
     function getAll() {
-        $.ajax({
+        /* $.ajax({
             method: 'GET',
             url: _config.api.invokeUrl + '/search',
             headers: {
@@ -27,11 +27,74 @@ var Crawler = window.Crawler || {};
                 console.error("Response: ", xhr.responseText);
                 alert("Error getting user's search requests:\n" + xhr.responseText);
             }
-        });
+        }); */
+        refreshGrid(testData());
+
+        function testData() {
+            return [
+                {
+                    SearchId: randomId(),
+                    UserEmail: "useinm@gmail.com",
+                    URL: "first_test_url",
+                    IsActive: true,
+                    Created: new Date(),
+                    Updated: new Date()
+                },
+                {
+                    SearchId: randomId(),
+                    UserEmail: "useinm@gmail.com",
+                    URL: "second_test_url",
+                    IsActive: true,
+                    Created: new Date(),
+                    Updated: new Date()
+                }
+            ];
+        }
+    }
+
+    var ItemViewModel = function(parent, data, isNew) {
+        this.isNew = ko.observable(isNew);
+        this.url = ko.observable(data.URL);
+        this.isActive = ko.observable(data.IsActive);
+        this.isDirty = ko.computed(
+            function() {
+                return this.url() != data.URL || this.isActive() != data.IsActive;
+            },
+            this);
+        var self = this;
+        this.save = function() {
+            data.URL = self.url();
+            data.IsActive = self.isActive();
+            if (self.isNew()) {
+                self.isNew(false);
+            }
+        }
+        this.remove = function() {
+            parent.items.remove(self);
+        }
+    }
+
+    var GridViewModel = function(data) {
+        var items = [];
+        for (var i = 0; i < data.length; i++) {
+            items.push(new ItemViewModel(this, data[i], false));
+        }
+        this.items = ko.observableArray(items);
+        var self = this;
+        this.addNew = function() {
+            var itemData = {
+                SearchId: randomId(),
+                URL: "",
+                IsActive: true,
+                Created: new Date(),
+                Updated: new Date()
+            };
+            self.items.push(new ItemViewModel(self, itemData, true));
+        }
     }
 
     function refreshGrid(result) {
-        // todo
+        ko.applyBindings(new GridViewModel(result));
     }
 
     function randomId() {
@@ -57,4 +120,4 @@ var Crawler = window.Crawler || {};
             getAll();
         }
     });
-}(jQuery));
+}(jQuery, ko));
