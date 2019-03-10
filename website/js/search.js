f1eaf2b25d9676ff163189557c828565b2bb3659
Usein Mambediiev <useinm@gmail.com>
2019-03-10 00:14:10 -0800

    working on search request editing UI

--- website/js/search.js
+++ website/js/search.js
@@ -13,74 +13,78 @@ var Crawler = window.Crawler || {};
         window.location.href = '/login.html';
     });
 
-    function getAll() {
-        /* $.ajax({
-            method: 'GET',
-            url: _config.api.invokeUrl + '/search',
-            headers: {
-                Authorization: authToken
-            },
-            contentType: 'application/json',
-            success: refreshGrid,
-            error: function (xhr, status, errorMessage) {
-                console.error("Error getting user's search requests: ", status, ", details: ", errorMessage);
-                console.error("Response: ", xhr.responseText);
-                alert("Error getting user's search requests:\n" + xhr.responseText);
-            }
-        }); */
-        refreshGrid(testData());
-
-        function testData() {
-            return [
-                {
-                    SearchId: randomId(),
-                    UserEmail: "useinm@gmail.com",
-                    URL: "first_test_url",
-                    IsActive: true,
-                    Created: new Date(),
-                    Updated: new Date()
-                },
-                {
-                    SearchId: randomId(),
-                    UserEmail: "useinm@gmail.com",
-                    URL: "second_test_url",
-                    IsActive: true,
-                    Created: new Date(),
-                    Updated: new Date()
-                }
-            ];
+    function randomId() {
+        var id = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for (var i = 0; i < 6; i++) {
+            id += possible.charAt(Math.floor(Math.random() * possible.length));
         }
+        return id;
     }
 
     var ItemViewModel = function(parent, data, isNew) {
-        this.isNew = ko.observable(isNew);
         this.url = ko.observable(data.URL);
         this.isActive = ko.observable(data.IsActive);
+        this.updated = ko.observable(data.Updated.toDateString());
+        this.isNew = ko.observable(isNew);
         this.isDirty = ko.computed(
             function() {
-                return this.url() != data.URL || this.isActive() != data.IsActive;
+                return this.url() !== data.URL || this.isActive() !== data.IsActive;
             },
             this);
         var self = this;
+
         this.save = function() {
             data.URL = self.url();
             data.IsActive = self.isActive();
-            if (self.isNew()) {
-                self.isNew(false);
-            }
-        }
+            
+            $.ajax({
+                method: self.isNew() ? 'PUT' : 'PATCH',
+                url: _config.api.invokeUrl + '/search/' + data.SearchId,
+                headers: {
+                    Authorization: authToken
+                },
+                data: data,
+                contentType: 'application/json',
+                success: parent.reload,
+                error: function (xhr, status, errorMessage) {
+                    console.error("Error saving user's search request: ", status, ", details: ", errorMessage);
+                    console.error("Response: ", xhr.responseText);
+                    alert("Error saving user's search request:\n" + xhr.responseText);
+                }
+            });
+        };
+
         this.remove = function() {
-            parent.items.remove(self);
-        }
+            $.ajax({
+                method: 'DELETE',
+                url: _config.api.invokeUrl + '/search/' + data.SearchId,
+                headers: {
+                    Authorization: authToken
+                },
+                contentType: 'application/json',
+                success: parent.reload,
+                error: function (xhr, status, errorMessage) {
+                    console.error("Error deleting user's search request: ", status, ", details: ", errorMessage);
+                    console.error("Response: ", xhr.responseText);
+                    alert("Error deleting user's search request:\n" + xhr.responseText);
+                }
+            });
+        };
     }
 
-    var GridViewModel = function(data) {
-        var items = [];
-        for (var i = 0; i < data.length; i++) {
-            items.push(new ItemViewModel(this, data[i], false));
-        }
-        this.items = ko.observableArray(items);
+    var GridViewModel = function() {
+        this.items = ko.observableArray([]);
         var self = this;
+
+        var refresh = function(data) {
+            var items = [];
+            for (var i = 0; i < data.length; i++) {
+                items.push(new ItemViewModel(self, data[i], false));
+            }
+            self.items(items);
+        }
+
         this.addNew = function() {
             var itemData = {
                 SearchId: randomId(),
@@ -91,19 +95,25 @@ var Crawler = window.Crawler || {};
             };
             self.items.push(new ItemViewModel(self, itemData, true));
         }
-    }
 
-    function refreshGrid(result) {
-        ko.applyBindings(new GridViewModel(result));
-    }
-
-    function randomId() {
-        var id = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for (var i = 0; i < 6; i++) {
-            id += possible.charAt(Math.floor(Math.random() * possible.length));
+        this.reload = function() {
+            $.ajax({
+                method: 'GET',
+                url: _config.api.invokeUrl + '/search',
+                headers: {
+                    Authorization: authToken
+                },
+                contentType: 'application/json',
+                success: refresh,
+                error: function (xhr, status, errorMessage) {
+                    console.error("Error getting user's search requests: ", status, ", details: ", errorMessage);
+                    console.error("Response: ", xhr.responseText);
+                    alert("Error getting user's search requests:\n" + xhr.responseText);
+                }
+            });
         }
-        return id;
+
+        this.reload();
     }
 
     $(function onDocReady() {
@@ -117,7 +127,7 @@ var Crawler = window.Crawler || {};
             $("#noApiMessage").show();
         }
         else {
-            getAll();
+            ko.applyBindings(new GridViewModel());
         }
     });
 }(jQuery, ko));
